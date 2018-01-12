04fe658b0f93a3fdd02f77ddba4a468400472f8c
Monica Roselind Panneer Selvam <monicap@itp.objectfrontier.com>
2018-01-12 18:33:29 +0530

    commit

---
+++ src/com/ofs/rest/Service.java
@@ -0,0 +1,21 @@
+package com.ofs.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class Service {
+
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+
+		String output = "Jersey says : " + msg;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+}
